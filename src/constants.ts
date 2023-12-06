27a7e7e273bcab231fcef0f2322a1b3aaf58ed7a
GMenegatto <gabrielmenegattogf@hotmail.com>
2023-12-06 00:15:37 -0300

    ssl

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const constants = {
-  API_URL_PREFFIX: 'http://ec2-18-218-230-236.us-east-2.compute.amazonaws.com/v1'
+  API_URL_PREFFIX: 'https://ec2-18-218-230-236.us-east-2.compute.amazonaws.com/v1'
 };
